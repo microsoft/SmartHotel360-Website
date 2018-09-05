38dcc634e8115f21b6105905c6d5d4e5926ee3cd
dsanz <dsanz@plainconcepts.com>
2018-09-05 16:15:05 +0200

    Add app insights with support for snapshot debugging

--- SmartHotel360.PublicWeb/Controllers/HomeController.cs
+++ SmartHotel360.PublicWeb/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using SmartHotel360.PublicWeb.Services;
 using SmartHotel360.PublicWeb.Models.Settings;
 using System;
+using Microsoft.ApplicationInsights;
 
 namespace SmartHotel360.PublicWeb.Controllers
 {
@@ -23,6 +24,21 @@ namespace SmartHotel360.PublicWeb.Controllers
         {
             ViewData["RequestId"] = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
             return View();
+        }
+
+        public IActionResult Exception([FromQuery] bool throwException = false)
+        {
+            try
+            {
+                if (throwException) ExceptionGenerator.GenerateException();
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                var wrappingException = new ApplicationException("This is a test exception", ex);
+
+                throw wrappingException;
+            }
         }
     }
 }
---
+++ SmartHotel360.PublicWeb/ExceptionGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartHotel360.PublicWeb
+{
+    public class ExceptionGenerator
+    {
+        public static int GenerateException()
+        {
+            var zero = 0;
+            var nonzero = 4;
+
+            var wrongCalculation = nonzero / zero;
+
+            return wrongCalculation;
+        }
+    }
+}
