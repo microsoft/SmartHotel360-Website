eec222532ff2bef13f71ff29a644b3f59006864a
Quique Fdez Guerra <quique@ckgrafico.com>
2018-09-12 16:46:48 +0200

    Add websockets to pets

--- SmartHotel360.PublicWeb/ClientApp/src/store/Pets.js
+++ SmartHotel360.PublicWeb/ClientApp/src/store/Pets.js
@@ -1,10 +1,12 @@
+import * as signalR from '@aspnet/signalr';
+import { settings } from '../Settings';
 const api = `/api/Pets`;
 
 export const Status = {
     None: 1,
     Ok: 2,
     Bad: 3
-}
+};
 
 export class PetInfo {
     constructor(base64) {
@@ -17,7 +19,8 @@ const initialState = {
     isThinking: false,
     id: null,
     image: null,
-    status: {approved: false, message: ''}
+    approved: false,
+    message: ''
 };
 
 function postImage(pet) {
@@ -28,54 +31,21 @@ function postImage(pet) {
         },
         body: JSON.stringify(pet)
     })
-        .then(response => response.json())
+    .then(response => response.json());
 
     return fetchTask;
 }
 
-const maximumAttempts = 100;
-const differenceTime = 400;
+function requestSignalRToken() {
+    let fetchTask = fetch(`${settings().petsConfig.api}/api/SignalRInfo`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => response.json());
 
-let startTime;
-let isGettingInfo;
-let attempts;
-let frame;
-
-function recursiveGet(id, resolve) {
-    let now = Date.now();
-    if (!isGettingInfo && (now - startTime > differenceTime)) {
-        isGettingInfo = true;
-        fetch(api + `?identifier=${id}`)
-            .then(response => response.json())
-            .then(status => {
-                isGettingInfo = false;
-
-                if (status.message !== '') {
-                    cancelAnimationFrame(frame);
-                    resolve(status);
-                } else {
-                    attempts++;
-                    if (attempts >= maximumAttempts) {
-                        cancelAnimationFrame(frame);
-                        resolve(status);
-                    }
-                }
-
-                startTime = now;
-            })
-    }
-
-    frame = requestAnimationFrame(() => recursiveGet(id, resolve));
-}
-
-function getStatus(id) {
-    startTime = Date.now();
-    isGettingInfo = false;
-    attempts = 0;
-
-    return new Promise(resolve => {
-        frame = requestAnimationFrame(() => recursiveGet(id, resolve));
-    });
+    return fetchTask;
 }
 
 export const actionCreators = {
@@ -83,18 +53,29 @@ export const actionCreators = {
         dispatch({ type: 'INIT_ACTION'});
     },
 
-    uploadPet: (pet) => (dispatch, getState) => {
-        postImage(pet)
-            .then(id => {
-                dispatch({ type: 'RECEIVE_PET_UPLOAD_ACTION', id: id });
+    uploadPet: (pet) => async (dispatch, getState) => {
+        const tokenInfo = await requestSignalRToken();
+        const options = {
+            accessTokenFactory: () => tokenInfo.accessKey
+        };
 
-                // Now start to get status
-                dispatch({ type: 'START_POOLING_ACTION' });
-                getStatus(id).then(status => {
-                    dispatch({ type: 'END_POOLING_ACTION', status: status });
-                });
-            });
+        const connection = new signalR.HubConnectionBuilder()
+            .withUrl(tokenInfo.endpoint, options)
+            .configureLogging(signalR.LogLevel.Information)
+            .build();
+
+        connection.on('ProcessDone', (data) => {
+            dispatch({ type: 'END_SOCKET', approved: data.accepted, message: data.message });
+        });
+        connection.onclose(() => console.log('server closed'));
+
         dispatch({ type: 'REQUEST_PET_UPLOAD_ACTION', image: pet.base64 });
+
+        const id = await postImage(pet);
+        dispatch({ type: 'RECEIVE_PET_UPLOAD_ACTION', id: id });
+  
+        dispatch({ type: 'START_SOCKET' });
+        connection.start().catch(console.error);
     }
 };
 
@@ -102,15 +83,15 @@ export const reducer = (state, action) => {
     switch (action.type) {
         case 'INIT_ACTION':
             return {
-                ...state, isUploading: false, isThinking: false, image: null, status: {approved: null, message: ''}};
+                ...state, isUploading: false, isThinking: false, image: null, approved: null, message: ''};
         case 'REQUEST_PET_UPLOAD_ACTION':
-            return { ...state, isUploading: true, isThinking: false, image: action.image, status: { approved: null, message: '' } };
+            return { ...state, isUploading: true, isThinking: false, image: action.image, approved: null, message: '' };
         case 'RECEIVE_PET_UPLOAD_ACTION':
             return { ...state, isUploading: false, isThinking: false, id: action.id };
-        case 'START_POOLING_ACTION':
+        case 'START_SOCKET':
             return { ...state, isUploading: false, isThinking: true };
-        case 'END_POOLING_ACTION':
-            return { ...state, isUploading: false, isThinking: false, image: null, status: action.status };
+        case 'END_SOCKET':
+            return { ...state, isUploading: false, isThinking: false, image: null, approved: action.approved, message: action.message };
         default:
             return state || { ...initialState };
     }
