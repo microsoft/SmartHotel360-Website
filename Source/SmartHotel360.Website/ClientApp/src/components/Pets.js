68142274786e2fe94b96776afff18ab572f5ae51
fpelaez <pelaezjr@outlook.com>
2018-11-18 20:53:43 -0800

    Fix website to make AZ function works

--- Source/SmartHotel360.Website/ClientApp/src/components/Pets.js
+++ Source/SmartHotel360.Website/ClientApp/src/components/Pets.js
@@ -40,8 +40,8 @@ class Pets extends Component {
             <div className='sh-pets-margin'></div>
             <h2 className='sh-pets-subtitle'>Do you want to know if your pet can accompany you?</h2>
 
-            <section className={'sh-uploader ' + (this.props.isThinking || this.props.isUploading ? 'is-loading' : 'is-empty') + ' ' + (this.props.approved === true ? 'is-ok' : '') + ' ' + (this.props.approved === false ? 'is-bad' : '')}
-                     onClick={this.onClickUploader}>
+            <section className={'sh-uploader ' + (this.props.isThinking || this.props.isUploading ? 'is-loading' : 'is-empty') + ' ' + (this.props.status.approved === true ? 'is-ok' : '') + ' ' + (this.props.status.approved === false ? 'is-bad' : '')}
+                onClick={this.onClickUploader}>
 
                 {this.props.image ? <div className='sh-uploader-image' style={{ backgroundImage: `url(${this.props.image})` }}></div> : <div className='sh-uploader-avatar'></div>}
 
@@ -50,19 +50,16 @@ class Pets extends Component {
                 </div>
             </section>
 
-            {!this.props.isThinking && !this.props.isUploading && this.props.approved === null ? <span className='sh-pets-smalltitle'>Click on the avatar to upload your picture</span> : <span></span>}
+            {!this.props.isThinking && !this.props.isUploading && this.props.status.approved === null ? <span className='sh-pets-smalltitle'>Click on the avatar to upload your picture</span> : <span></span>}
             {this.props.isUploading ? <span className='sh-pets-smalltitle'>Uploading the image...</span> : <span></span>}
             {this.props.isThinking ? <span className='sh-pets-smalltitle'>Processing the image...</span> : <span></span>}
 
-            {this.props.approved === true ? <span className='sh-pets-smalltitle is-ok'>Your pet looks like a {this.props.message} and is accepted.</span> : <span></span>}
-            {this.props.approved === false ? <span className='sh-pets-smalltitle is-bad'>Sorry your pet seems to be a {this.props.message} and we can't allow it.</span> : <span></span>}
-
+            {this.props.status.approved === true ? <span className='sh-pets-smalltitle is-ok'>Your pet looks like a {this.props.status.message} and is accepted :)</span> : <span></span>}
+            {this.props.status.approved === false ? <span className='sh-pets-smalltitle is-bad'>Sorry your pet seems to be a {this.props.status.message} and we can't afford it :(</span> : <span></span>}
             <input className='is-hidden' ref='nativeUpload' type='file' onChange={this.onFileChange} />
-
         </div>;
     }
 }
-
 export default connect(
     state => state.pets,
     dispatch => bindActionCreators(PetsStore.actionCreators, dispatch)
--- Source/SmartHotel360.Website/ClientApp/src/store/Pets.js
+++ Source/SmartHotel360.Website/ClientApp/src/store/Pets.js
@@ -1,12 +1,10 @@
-import * as signalR from '@aspnet/signalr';
-import { settings } from '../Settings';
 const api = `/api/Pets`;
 
 export const Status = {
     None: 1,
     Ok: 2,
     Bad: 3
-};
+}
 
 export class PetInfo {
     constructor(base64) {
@@ -19,8 +17,7 @@ const initialState = {
     isThinking: false,
     id: null,
     image: null,
-    approved: false,
-    message: ''
+    status: { approved: false, message: '' }
 };
 
 function postImage(pet) {
@@ -31,51 +28,73 @@ function postImage(pet) {
         },
         body: JSON.stringify(pet)
     })
-    .then(response => response.json());
+        .then(response => response.json())
 
     return fetchTask;
 }
 
-function requestSignalRToken() {
-    let fetchTask = fetch(`${settings().petsConfig.api}/api/SignalRInfo`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-    .then(response => response.json());
+const maximumAttempts = 100;
+const differenceTime = 400;
 
-    return fetchTask;
+let startTime;
+let isGettingInfo;
+let attempts;
+let frame;
+
+function recursiveGet(id, resolve) {
+    let now = Date.now();
+    if (!isGettingInfo && (now - startTime > differenceTime)) {
+        isGettingInfo = true;
+        fetch(api + `?identifier=${id}`)
+            .then(response => response.json())
+            .then(status => {
+                isGettingInfo = false;
+
+                if (status.message !== '') {
+                    cancelAnimationFrame(frame);
+                    resolve(status);
+                } else {
+                    attempts++;
+                    if (attempts >= maximumAttempts) {
+                        cancelAnimationFrame(frame);
+                        resolve(status);
+                    }
+                }
+
+                startTime = now;
+            })
+    }
+
+    frame = requestAnimationFrame(() => recursiveGet(id, resolve));
+}
+
+function getStatus(id) {
+    startTime = Date.now();
+    isGettingInfo = false;
+    attempts = 0;
+
+    return new Promise(resolve => {
+        frame = requestAnimationFrame(() => recursiveGet(id, resolve));
+    });
 }
 
 export const actionCreators = {
     init: () => (dispatch, getState) => {
-        dispatch({ type: 'INIT_ACTION'});
+        dispatch({ type: 'INIT_ACTION' });
     },
 
-    uploadPet: (pet) => async (dispatch, getState) => {
-        const tokenInfo = await requestSignalRToken();
-        const options = {
-            accessTokenFactory: () => tokenInfo.accessKey
-        };
+    uploadPet: (pet) => (dispatch, getState) => {
+        postImage(pet)
+            .then(id => {
+                dispatch({ type: 'RECEIVE_PET_UPLOAD_ACTION', id: id });
 
-        const connection = new signalR.HubConnectionBuilder()
-            .withUrl(tokenInfo.endpoint, options)
-            .configureLogging(signalR.LogLevel.Information)
-            .build();
-
-        connection.on('ProcessDone', (data) => {
-            dispatch({ type: 'END_SOCKET', approved: data.accepted, message: data.message });
-        });
-        connection.onclose(() => console.log('server closed'));
-
+                // Now start to get status
+                dispatch({ type: 'START_POOLING_ACTION' });
+                getStatus(id).then(status => {
+                    dispatch({ type: 'END_POOLING_ACTION', status: status });
+                });
+            });
         dispatch({ type: 'REQUEST_PET_UPLOAD_ACTION', image: pet.base64 });
-
-        const id = await postImage(pet);
-        dispatch({ type: 'RECEIVE_PET_UPLOAD_ACTION', id: id });
-  
-        dispatch({ type: 'START_SOCKET' });
-        connection.start().catch(console.error);
     }
 };
 
@@ -83,15 +102,16 @@ export const reducer = (state, action) => {
     switch (action.type) {
         case 'INIT_ACTION':
             return {
-                ...state, isUploading: false, isThinking: false, image: null, approved: null, message: ''};
+                ...state, isUploading: false, isThinking: false, image: null, status: { approved: null, message: '' }
+            };
         case 'REQUEST_PET_UPLOAD_ACTION':
-            return { ...state, isUploading: true, isThinking: false, image: action.image, approved: null, message: '' };
+            return { ...state, isUploading: true, isThinking: false, image: action.image, status: { approved: null, message: '' } };
         case 'RECEIVE_PET_UPLOAD_ACTION':
             return { ...state, isUploading: false, isThinking: false, id: action.id };
-        case 'START_SOCKET':
+        case 'START_POOLING_ACTION':
             return { ...state, isUploading: false, isThinking: true };
-        case 'END_SOCKET':
-            return { ...state, isUploading: false, isThinking: false, image: null, approved: action.approved, message: action.message };
+        case 'END_POOLING_ACTION':
+            return { ...state, isUploading: false, isThinking: false, image: null, status: action.status };
         default:
             return state || { ...initialState };
     }
